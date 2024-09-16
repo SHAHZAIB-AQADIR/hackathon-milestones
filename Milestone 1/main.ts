8e26ff0efe1babe682957533458ce8c00a3bcc91
SHAHZAIB-AQADIR <165094105+SHAHZAIB-AQADIR@users.noreply.github.com>
2024-09-17 01:05:09 +0500

    Add files via upload

---
+++ Milestone 1/main.ts
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('Toggle-Skills') as HTMLButtonElement
+const skills = document.getElementById('Skills') as HTMLElement
+
+
+toggleButton.addEventListener('click',()=>{
+    if(skills.style.display === 'none'){
+        skills.style.display = 'block'
+    }else {
+        skills.style.display = 'none'
+    }
+});
+
+
